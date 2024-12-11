12068e6943739b6d4118d4fd2a0e6674a2583e38
jberm0 <jmberman12@gmail.com>
2024-12-11 10:46:12 +0000

    add tags to rg

--- main.tf
+++ main.tf
@@ -20,13 +20,17 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westus2"
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+  }
 }
 
 # adding vnet to be contained within the rg
 # terraform recognises the dependency and creates the resources in order
 resource "azurerm_virtual_network" "vnet" {
-  name    = "myTFVnet"
-  address_space = ["10.0.0.0/16"]
-  location = "westus2"
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
   resource_group_name = azurerm_resource_group.rg.name
 }
